38716abdcb544cdbb6018e1c48ec4090c357e420
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2018-12-12 23:02:21 -0500

    added 2 other methods (hasNext and setNext) becuase they were necessary - I think- to creating MyLinkedList

--- Node.java
+++ Node.java
@@ -2,10 +2,25 @@ public class Node {
   private int data;
   private Node next,prev;
 
-  public Node(Node setPrev, Node setNext, int setData) {
+  public Node(Node setPrev, int setData) { // should the next node be part of the constructor?
     data = setData;
-    next = setNext;
+    //next = setNext;
     prev = setPrev;
+  }
+
+  //NOT SURE IF WE WERE SUPPOSED TO DO THIS
+  //this method retroactively sets the next node.
+  public void setNext(Node setNext) {
+    next = setNext;
+  }
+
+  //
+  public boolean hasNext() {
+    if (next != null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public static void main(String[] args) {
