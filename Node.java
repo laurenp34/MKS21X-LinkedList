e16f42be58c5766aa282f960ef931c3652bf4030
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2018-12-12 23:05:38 -0500

    tested nodes and my methods all passed (simple tests). Also, changed the constructor to only take a value, instead added a setPrev method.

--- Node.java
+++ Node.java
@@ -2,16 +2,20 @@ public class Node {
   private int data;
   private Node next,prev;
 
-  public Node(Node setPrev, int setData) { // should the next node be part of the constructor?
+  public Node(int setData) { // should the next node be part of the constructor?
     data = setData;
     //next = setNext;
-    prev = setPrev;
+    //prev = setPrev;
   }
 
   //NOT SURE IF WE WERE SUPPOSED TO DO THIS
   //this method retroactively sets the next node.
   public void setNext(Node setNext) {
     next = setNext;
+  }
+
+  public void setPrev(Node setPrev) {
+    prev = setPrev;
   }
 
   //
@@ -24,6 +28,10 @@ public class Node {
   }
 
   public static void main(String[] args) {
-    
+    Node test = new Node(4);
+    Node test2 = new Node(2);
+    test.setNext(test2);
+    System.out.println(test.hasNext());
+    System.out.println(test2.hasNext());
   }
 }
