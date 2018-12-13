646ec08357f272ce4d11b98241c7b324c5f1b611
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2018-12-12 22:51:28 -0500

    adeded a constructor to Node

--- Node.java
+++ Node.java
@@ -2,7 +2,13 @@ public class Node {
   private int data;
   private Node next,prev;
 
-  public static void main(String[] args) {
+  public Node(Node setPrev, Node setNext, int setData) {
+    data = setData;
+    next = setNext;
+    prev = setPrev;
+  }
 
+  public static void main(String[] args) {
+    
   }
 }
